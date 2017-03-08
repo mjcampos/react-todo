6da6ac7a9c38b70e519b5eafa08b8bd4faa14ff9
Michael Campos <camposmj89@gmail.com>
2017-03-08 06:34:15 -0800

    Modified template to include CSS stylesheet and foundation

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ var express = require('express');
 
 // Create our App
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+	if(req.headers['x-forwarded-proto'] === 'https') {
+		res.redirect('http://' + req.hostname + req.url);
+	} else {
+		next();
+	}
+});
 
 app.use(express.static('public'));
 
-var port = 3000;
-
-app.listen(port, function() {
-	console.log('Express server is up at port ' + port);
+app.listen(PORT, function() {
+	console.log('Express server is up at port ' + PORT);
 })
