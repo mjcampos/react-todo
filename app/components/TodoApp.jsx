799ea5a9c93268c0752e9770c521474f60ee64d4
Michael Campos <camposmj89@gmail.com>
2017-03-19 21:01:55 -0700

    Refactor app to work with localStorage

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -4,50 +4,16 @@ var moment = require('moment');
 import TodoList from 'TodoList';
 import AddTodo from 'AddTodo';
 import Search from 'Search';
-var TodoAPI = require('TodoAPI');
 
 var TodoApp = React.createClass({
-	getInitialState: function() {
-		return {
-			showCompleted: false,
-			searchText: '',
-			todos: TodoAPI.getTodos()
-		};
-	},
-	componentDidUpdate: function() {
-		TodoAPI.setTodos(this.state.todos);
-	},
-	handleAddTodo: function(text) {
-		this.setState({
-			todos: [
-				...this.state.todos,
-				{
-					id: uuid(),
-					text: text,
-					completed: false,
-					createdAt: moment().unix(),
-					completedAt: undefined
-				}
-			]
-		});
-	},
-	handleSearch: function(showCompleted, searchText) {
-		this.setState({
-			showCompleted: showCompleted,
-			searchText: searchText.toLowerCase()
-		});
-	},
 	render: function() {
-		var {todos, showCompleted, searchText} = this.state;
-		var filterTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
-
 		return (
 			<div>
 				<h1 className="page-title">Todo App</h1>
 				<div className="row">
 					<div className="column small-centered small-11 medium-6 large-5">
 						<div className="container">
-							<Search onSearch={this.handleSearch}/>
+							<Search/>
 							<TodoList/>
 							<AddTodo/>
 						</div>
--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -22,6 +22,23 @@ describe('Actions', () => {
 		expect(res).toEqual(action);
 	});
 
+	it('should generate add todos action', () => {
+		var todos = [{
+			id: 1,
+			text: 'anything',
+			completed: false,
+			completedAt: undefined,
+			createdAt: 33000
+		}];
+		var action = {
+			type: 'ADD_TODOS',
+			todos
+		};
+		var res = actions.addTodos(todos);
+
+		expect(res).toEqual(action);
+	});
+
 	it('should generate toggle show completed action', () => {
 		var action = {
 			type: 'TOGGLE_SHOW_COMPLETED'
@@ -39,5 +56,5 @@ describe('Actions', () => {
 		var res = actions.toggleTodo(action.id);
 
 		expect(res).toEqual(action);
-	})
+	});
 });
